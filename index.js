e1b70cda95a56968799fd7e9bb6f886e04592848
Satyam Tiwari <satyamtiwari0734@gmail.com>
2025-08-01 12:28:39 +0530

    github added

--- index.js
+++ index.js
@@ -5,6 +5,42 @@ const app = express();
 
 const port = 4000;
 
+const githubData = {
+  "login": "hiteshchoudhary",
+  "id": 11613311,
+  "node_id": "MDQ6VXNlcjExNjEzMzEx",
+  "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/hiteshchoudhary",
+  "html_url": "https://github.com/hiteshchoudhary",
+  "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
+  "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+  "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
+  "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
+  "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
+  "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Hitesh Choudhary",
+  "company": null,
+  "blog": "https://hitesh.ai",
+  "location": "India",
+  "email": null,
+  "hireable": null,
+  "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+  "twitter_username": "hiteshdotcom",
+  "public_repos": 111,
+  "public_gists": 5,
+  "followers": 47096,
+  "following": 0,
+  "created_at": "2015-03-23T13:03:25Z",
+  "updated_at": "2025-07-28T07:05:10Z"
+}
+
 app.get('/', (req,res) => {
     res.send("Hello World");
 })
@@ -19,6 +55,10 @@ app.get('/login', (req,res) => {
 
 app.get('/chai', (req,res) => {
     res.send("<h2>Chai aur Code </h2>");
+})
+
+app.get('/github' , (req ,res) => {
+    res.json(githubData);
 })
 
 app.listen(process.env.PORT, () => {
